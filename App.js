0ca282824c61f02b3c757fb84cbd171d3de3ce8d
amolkapadi <amolakapadi@gmail.com>
2023-08-02 06:59:10 +0530

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,69 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import Constants from 'expo-constants'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+
+import Home from './component/Home'
+import About from './component/About'
+import Profile from './component/Profile'
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    
       <StatusBar style="auto" />
+      <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen
+          name="Home1"
+          component={Home}
+          options={{
+            tabBarIcon:({color,size})=>(
+              <Ionicons name="home" size={size} color={color}  />
+            )
+          }}
+          >
+
+          </Tab.Screen>
+          <Tab.Screen
+          name="About"
+          component={About}
+          options={{
+            tabBarIcon:({color,size})=>(
+              <Ionicons name="information-circle" size={size} color={color}  />
+            )
+          }}
+          >
+
+          </Tab.Screen>
+          <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarIcon:({color,size})=>(
+              <Ionicons name="person" size={size} color={color}  />
+            )
+          }}
+          >
+
+          </Tab.Screen>
+        </Tab.Navigator>
+
+
+
+
+
+
+
+
+        
+      </NavigationContainer>
+
     </View>
   );
 }
@@ -13,8 +71,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:Constants.statusBarHeight,
   },
 });
